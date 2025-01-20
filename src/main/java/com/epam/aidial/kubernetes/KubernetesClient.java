9839ce2d5e8a746b4a8ab3adb599b995da5ad205
Oleksii-Klimov <133792808+Oleksii-Klimov@users.noreply.github.com>
2025-01-20 09:31:23 +0000

    fix: delete job pods (#21)

--- src/main/java/com/epam/aidial/kubernetes/KubernetesClient.java
+++ src/main/java/com/epam/aidial/kubernetes/KubernetesClient.java
@@ -34,6 +34,7 @@ public class KubernetesClient {
     };
     private static final TypeToken<Watch.Response<V1Service>> SERVICE_TYPE_TOKEN = new TypeToken<>() {
     };
+    private static final String BACKGROUND_POLICY = "Background";
     private static final String FOREGROUND_POLICY = "Foreground";
     private static final String NAME_SELECTOR_PREFIX = "metadata.name=";
 
@@ -203,6 +204,8 @@ public class KubernetesClient {
             log.info("Deleting job {}", name);
             try {
                 batchV1Api.deleteNamespacedJob(name, namespace)
+                        .propagationPolicy(BACKGROUND_POLICY)
+                        .gracePeriodSeconds(0)
                         .executeAsync(new NoProgressApiCallback<>() {
                             @Override
                             public void onFailure(ApiException e, int i, Map<String, List<String>> map) {
